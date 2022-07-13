f5a33b767b573b1ed99688febf9baf92159c15b0
Matt Muoio <mmuoio@gmail.com>
2022-07-13 15:49:32 -0400

    adding migration

--- website/manage.py
+++ website/manage.py
@@ -1,9 +1,17 @@
-from flask_script import Manager
+from app import app, db
+from .models import User, Player, Alias, Game, Payment, Url
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-from app import app
-from . import db
-migrate = Migrate(app, db)
+
 manager = Manager(app)
+migrate = Migrate(app,db)
+
 manager.add_command('db', MigrateCommand)
+manager.add_command('server', Server)
+
+@manager.shell
+def make_shell_context():
+	return dict(app=app, db=db, Game=Game, User=User, Player=Player, Alias=Alias, Payment=Payment, Url=Url) # Add Game model to the dict
+
 if __name__ == '__main__':
-    manager.run()
+	manager.run()
