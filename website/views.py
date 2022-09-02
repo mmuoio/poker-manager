5c1a0c45c332c5eae4399882872c2de878850cf8
Matt Muoio <mmuoio@gmail.com>
2022-09-02 15:25:45 -0400

    fixing AWS security

--- website/views.py
+++ website/views.py
@@ -12,8 +12,8 @@ import os
 
 
 s3 = boto3.client('s3',
-                    aws_access_key_id=os.environ('AWS_ACCESS_KEY_ID'),
-                    aws_secret_access_key= os.environ('AWS_SECRET_ACCESS_KEY'),
+                    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                    aws_secret_access_key= os.getenv('AWS_SECRET_ACCESS_KEY'),
 					region_name='us-east-1'
                      )
 BUCKET_NAME='pokermanager'
